159f9d8f80d481ffd5f19a11824e7907a48b3773
Tomaz Karer <tomaz@karer.si>
2021-10-04 05:35:29 +0200

    delo

--- neovim/lua/plugins-config/init.lua
+++ neovim/lua/plugins-config/init.lua
@@ -1,4 +1,4 @@
 require('plugins-config.nvim-tree-config')
 require('plugins-config.fzf-config')
-require('plugins-config.lspconfig-config')
+require('plugins-config.lsp-configs')
 require('plugins-config.vimwiki-config')
---
+++ neovim/lua/plugins-config/lsp-configs.lua
@@ -0,0 +1,1 @@
+require('lua-language-server')
