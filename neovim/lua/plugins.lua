a87b4e8aaa42b917ec07e66054b1fcf7f8d4f223
Tomaz Karer <tomaz@karer.si>
2021-10-23 21:59:38 +0200

    lualine relocation version of plugin

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -51,7 +51,7 @@ return require("packer").startup(function(use)
 --        }
 
 	use {
-          'shadmansaleh/lualine.nvim',
+          'nvim-lualine/lualine.nvim',
           requires = {'kyazdani42/nvim-web-devicons', opt = true}
         }
 
